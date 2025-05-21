2e1176f4d346bd8a6e0c1354f8254de8b629f8d2
CretuRobert <cretu_robert96@yahoo.com>
2025-05-21 17:18:11 +0300

    Add files via upload

---
+++ ProgrammingClubAPI/ProgrammingClubAPI/Models/MembershipType.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammingClubAPI.Models
+{
+    public class MembershipType
+    {
+        public Guid IDMembershipType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int SubscriptionLength { get; set; }
+    }
+}
