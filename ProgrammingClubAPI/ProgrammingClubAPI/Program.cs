7ec8678ea1b5258ad4a77675527bd724b723265d
CretuRobert <cretu_robert96@yahoo.com>
2025-06-23 18:42:12 +0300

    Add files via upload

--- ProgrammingClubAPI/ProgrammingClubAPI/Program.cs
+++ ProgrammingClubAPI/ProgrammingClubAPI/Program.cs
@@ -1,3 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProgrammingClubAPI.DataContext;
+using ProgrammingClubAPI.Repositories;
+using ProgrammingClubAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Reflection;
+using ProgrammingClubAPI.Helpers;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,18 +15,137 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+  
+    // our api is using jwt authentication
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+    });
+    // all endpoints are protected by [Authorize attribute]
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
 builder.Services.AddSwaggerGen();
 
+//inregistram clasa ProgrammingClubDataContext in containerul de dependente 
+builder.Services.AddDbContext<ProgrammingClubDataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<ProgrammingClubAuthDataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionAuth")));
+
+
+//Transient = de fiecare data cand se cere o instanta a clasei, se va crea una noua
+//Scoped = se va crea o instanta a clasei pentru fiecare request HTTP
+builder.Services.AddTransient<IMembersRepository, MembersRepository>();
+builder.Services.AddTransient<IMembersService, MembersService>();
+builder.Services.AddTransient<ITokenService, TokenService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddTransient<IAnnouncementsRepository, AnnouncementsRepository>();
+builder.Services.AddTransient<IAnnouncementsService, AnnouncementsService>();
+
+builder.Logging.AddLog4Net("log4net.config");
+
+//gestinare utilizatori user
+builder.Services.AddIdentityCore<IdentityUser>().
+    AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("ProgrammingClubAuthentication")
+    .AddEntityFrameworkStores<ProgrammingClubAuthDataContext>()
+    .AddDefaultTokenProviders();
+
+//reguli validare pass
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+});
+
+// Configurare autentificare JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+// autromated versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true; // Allows clients to see the API versions
+    options.AssumeDefaultVersionWhenUnspecified = true; // Default version if not specified
+    options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0); // Default version
+});
+
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV"; // Format for versioning
+    options.SubstituteApiVersionInUrl = true; // Substitute version in URL
+});
+
+builder.Services.ConfigureOptions<ConfigureSwagger>();
+
+// Register all handlers from assbly uri where Program class is located
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+builder.Services.AddMemoryCache(); // Add memory cache for caching
+
 var app = builder.Build();
+
+var versionDescriptionsProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var item in versionDescriptionsProvider.ApiVersionDescriptions)
+
+        {
+            options.SwaggerEndpoint($"/swagger/{item.GroupName}/swagger.json", item.GroupName.ToUpperInvariant());
+        }
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
